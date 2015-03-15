78bcc43cd652ab9e9d21140fa4ca336370f1776c
Yaroslav Filyk <yarfilyk@gmail.com>
2015-03-15 11:10:42 +0200

    Authentication tuning

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -1,13 +1,7 @@
 class OptionsController < ApplicationController
-
+  
   def pick
     @option = Option.find(params[:id])
-    @option.pick_by current_user
-
-    respond_to do |format|
-	    format.js
-	    format.html { render :nothing => true, :notice => 'Picked!' }
-	  end
+    current_user ? @option.pick_by(current_user) : render(js: "window.location = #{new_user_session_path.to_json}")
   end
-
  end

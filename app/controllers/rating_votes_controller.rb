2a68c06eb2ea5c6709d8eb2247b3f18a8f6ed530
Yaroslav Filyk <yarfilyk@gmail.com>
2015-03-16 12:15:44 +0200

    login fixes

--- app/controllers/rating_votes_controller.rb
+++ app/controllers/rating_votes_controller.rb
@@ -1,4 +1,6 @@
 class RatingVotesController < ApplicationController
+  before_action :check_user
+
   def create
     @poll = Poll.find(params[:poll_id])
     vote = @poll.rating_votes.find_by(user_id: current_user.id)
@@ -9,5 +11,10 @@ class RatingVotesController < ApplicationController
       RatingVote.create(poll_id: params[:poll_id], user_id: current_user.id, direction: params[:direction])
       @poll.vote(params[:direction])
     end    
+  end
+
+  private
+  def check_user
+    render(js: "window.location = #{new_user_session_path.to_json}") unless current_user
   end
 end
